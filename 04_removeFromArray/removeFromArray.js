59804fca805450420d1645afd3a4b7196cad70ad
ViniciusLucca <vinilucca7304@gmail.com>
2025-02-28 18:12:42 -0300

    Update removefromArray.js
    - Passes the first 6 tests, but loops infinitely on the 7th

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(_array) {
+    a_length = arguments.length;
+    for (let i = 0; i < _array.length; i++){
+        for (let j = 1; j < a_length; j++)
+        {
+            if(_array[i] === arguments[j]){
+                _array.splice(_array[i - 1], 1);
+                i = i - 1;
+            }
+        }
+    }
+    return _array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// sou lindo
