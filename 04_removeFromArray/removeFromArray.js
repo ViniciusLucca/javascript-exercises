b896071d06b2e23815361852f0ff7213fe16133c
ViniciusLucca <vinilucca7304@gmail.com>
2025-02-28 19:31:35 -0300

    Fix removeFromArray method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,17 @@
 const removeFromArray = function(_array) {
-    a_length = arguments.length;
-    for (let i = 0; i < _array.length; i++){
+    let a_length = arguments.length;
+    let result = [];
+    startloop : for (let i = 0; i < _array.length; i++){
         for (let j = 1; j < a_length; j++)
         {
             if(_array[i] === arguments[j]){
-                _array.splice(_array[i - 1], 1);
-                i = i - 1;
+                continue startloop;
             }
         }
+        result.push(_array[i]);
     }
-    return _array;
+    return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
-// sou lindo
