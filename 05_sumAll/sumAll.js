38ab34ccfd7947661e92286aac5b2433e03a8608
ViniciusLucca <vinilucca7304@gmail.com>
2025-02-28 21:25:19 -0300

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1,  num2, ...args) {
+    let result = 0;
+    if (num1 > num2) [num1, num2] = [num2, num1];
+    for (let i in [num1, num2]){
+        if ([num1, num2][i] < 0 ||
+            [num1, num2][i] !== +([num1, num2][i]) ||
+            [num1, num2][i] % 1 != 0
+        )
+        {
+            return 'ERROR';
+        }
+    }
+    for (num2; num2 >= num1; num2--){
+        result += num2;
+    }
+    return result;
 };
 
 // Do not edit below this line
