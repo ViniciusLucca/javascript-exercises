180f534c2e94a453e5163721af512fc3e2557d1d
ViniciusLucca <vinilucca7304@gmail.com>
2025-03-26 10:07:38 -0300

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = Array.from(str);
+    // let punctuation = [...("!,.")]
+    str = str.filter((item) => item != '!' && item != ',' && item != '.' && item != ' ');
+    let reverseStr = str.toReversed();
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != reverseStr[i]) return false
+    }
+    return true;
 };
 
 // Do not edit below this line
