4d470afdf1031b58841b9f8cd423e9918dc87e27
ViniciusLucca <vinilucca7304@gmail.com>
2025-03-26 11:13:03 -0300

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    if(num < 0 || num == NaN) return "OOPS";
+    if(num == 0) return 0;
+    if(num < 3) return 1;
+    
+    let fiboArray = [0, 1, 1];
+    let result = 0;
+    for (let i = 0; i < num - 1; i++)
+    {
+        fiboArray[2] = fiboArray[1] + fiboArray[0];
+        fiboArray[0] = fiboArray[1];
+        fiboArray[1] = fiboArray[2];
+    }
+    return fiboArray[2];
 
 };
 
