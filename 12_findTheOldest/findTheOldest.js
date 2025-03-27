93d7cc62837e50f36396673a8211073169269b27
ViniciusLucca <vinilucca7304@gmail.com>
2025-03-27 16:31:11 -0300

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleObj) {
+    console.log(peopleObj);
+    let oldest = 0;
+    let personObj;
+    let currentYear = new Date().getFullYear();
+    peopleObj.forEach(p => {
+        let age = (p['yearOfDeath'] ? p['yearOfDeath'] : currentYear) - p['yearOfBirth'];
+        if (age > oldest){
+            oldest = age;
+            console.log(age);
+        }
+    });
 
+    peopleObj.forEach(p => {
+        let age = (p['yearOfDeath'] ? p['yearOfDeath'] : currentYear) - p['yearOfBirth'];
+        if(age == oldest){
+            console.log("p: ", p);
+            personObj = p;
+        }
+    })
+
+    return personObj;
 };
 
 // Do not edit below this line
